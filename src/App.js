5fe474737b453ab595394ae45591be690dc547fb
MohammadWaqas908 <66936406+MohammadWaqas908@users.noreply.github.com>
2020-10-21 14:13:58 +0500

    Update App.js
    updated App.js

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.<br>
-          Edited Mohammad Waqas
+          Edit <code>src/App.js</code> and save to reload.
+          Edited By Mohammad Waqas
         </p>
         <a
           className="App-link"
